3b3094eb8427b6348748750bc5bf8933deb16355
Cam Spiers <camspiers@gmail.com>
2013-04-08 10:25:55 +1200

    FEATURE: Allow the specification of compiler pass configs

--- src/Camspiers/DependencyInjection/SharedContainerFactory.php
+++ src/Camspiers/DependencyInjection/SharedContainerFactory.php
@@ -2,96 +2,161 @@
 
 namespace Camspiers\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Container;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
-use Symfony\Component\DependencyInjection\Loader\FileLoader;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Config\FileLocator;
 use RuntimeException;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
+/**
+ * Class SharedContainerFactory
+ * @package Camspiers\DependencyInjection
+ */
 class SharedContainerFactory
 {
+    /**
+     * @var array
+     */
     protected static $extensions = array();
+    /**
+     * @var array
+     */
     protected static $compilerPasses = array();
-
-    public static function requireExtensionConfigs($path, $pattern = '*', $name = '_extensions.php')
+    /**
+     * Require a bunch of files specified by path patterns
+     *
+     * E.g.
+     * SharedContainerFactory::requireExtensionConfigs(
+     *     array(
+     *         __DIR__ . '/*\/_extensions.php'
+     *     )
+     * );
+     * @param array $patterns
+     */
+    public static function requireExtensionConfigs(array $patterns = array())
     {
-        $path = realpath($path);
-        if (file_exists($path)) {
-            foreach (glob("$path/$pattern/$name") as $file) {
+        foreach ($patterns as $pattern) {
+            foreach (glob($pattern) as $file) {
                 require_once $file;
             }
         }
     }
-
+    /**
+     * Check if an extension exists
+     * @param $alias
+     * @return bool
+     */
     public static function hasExtension($alias)
     {
         return isset(self::$extensions[$alias]);
     }
-
+    /**
+     * Add an extension
+     * @param ExtensionInterface $extension
+     * @throws \RuntimeException
+     */
     public static function addExtension(ExtensionInterface $extension)
     {
-        $alias = $extension->getAlias();
-        if (!self::hasExtension($alias)) {
+        if (!self::hasExtension($alias = $extension->getAlias())) {
             self::$extensions[$alias] = $extension;
         } else {
             throw new RuntimeException("Extension with alias '$alias' has already been added");
-            
         }
     }
-
+    /**
+     * Replace an extension
+     * @param ExtensionInterface $extension
+     */
     public static function replaceExtension(ExtensionInterface $extension)
     {
-        self::$extensions[$alias] = $extension;
+        self::$extensions[$extension->getAlias()] = $extension;
     }
-
+    /**
+     * Remove a specified extension
+     * @param $alias
+     */
     public static function removeExtension($alias)
     {
         if (self::hasExtension($alias)) {
             unset(self::$extensions[$alias]);
         }
     }
-
+    /**
+     * Remove all extensions
+     */
     public static function clearExtensions()
     {
         self::$extensions = array();
     }
-
+    /**
+     * Check if a compiler pass exists
+     * @param $name
+     * @return bool
+     */
     public static function hasCompilerPass($name)
     {
         return isset(self::$compilerPasses[$name]);
     }
-
-    public static function addCompilerPass(CompilerPassInterface $compilerPass)
-    {
-        $name = get_class($compilerPass);
-        if (!self::hasCompilerPass($name)) {
-            self::$compilerPasses[$name] = $compilerPass;
+    /**
+     * Add a compiler pass
+     * @param CompilerPassInterface $compilerPass
+     * @param string                $type
+     * @throws \RuntimeException
+     */
+    public static function addCompilerPass(
+        CompilerPassInterface $compilerPass,
+        $type = PassConfig::TYPE_BEFORE_OPTIMIZATION
+    ) {
+        if (!self::hasCompilerPass($name = get_class($compilerPass))) {
+            self::$compilerPasses[$name] = array(
+                $compilerPass,
+                $type
+            );
         } else {
             throw new RuntimeException("Compiler pass of classname '$name' has already been added");
         }
     }
-
-    public static function replaceCompilerPass(CompilerPassInterface $compilerPass)
-    {
-        self::$compilerPasses[get_class($compilerPass)] = $compilerPass;
+    /**
+     * Replace a compiler pass
+     * @param CompilerPassInterface $compilerPass
+     * @param string                $type
+     */
+    public static function replaceCompilerPass(
+        CompilerPassInterface $compilerPass,
+        $type = PassConfig::TYPE_BEFORE_OPTIMIZATION
+    ) {
+        self::$compilerPasses[get_class($compilerPass)] = array(
+            $compilerPass,
+            $type
+        );
     }
-
+    /**
+     * Remove a specified compiler pass
+     * @param $name
+     */
     public static function removeCompilerPass($name)
     {
         if (self::hasCompilerPass($name)) {
             unset(self::$compilerPasses[$name]);
         }
     }
-
+    /**
+     * Clear the compiler passes
+     */
     public static function clearCompilerPasses()
     {
         self::$compilerPasses = array();
     }
-
+    /**
+     * Create a container based on the static configuration of this class
+     * @param array $parameters
+     * @param bool  $servicesLocation
+     * @return ContainerBuilder
+     */
     public static function createContainer(
         array $parameters = array(),
         $servicesLocation = false
@@ -103,7 +168,7 @@ class SharedContainerFactory
         }
 
         foreach (self::$compilerPasses as $compilerPass) {
-            $container->addCompilerPass($compilerPass);
+            $container->addCompilerPass($compilerPass[0], $compilerPass[1]);
         }
 
         if ($servicesLocation && file_exists($servicesLocation)) {
@@ -118,7 +183,13 @@ class SharedContainerFactory
 
         return $container;
     }
-
+    /**
+     * Dump a container
+     * @param Container $container
+     * @param           $class
+     * @param           $location
+     * @throws \RuntimeException
+     */
     public static function dumpContainer(
         Container $container,
         $class,
